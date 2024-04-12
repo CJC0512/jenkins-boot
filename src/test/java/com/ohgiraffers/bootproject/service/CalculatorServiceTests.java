560eda5fa3a470e60c91fdfeda0cb0d4af0a20e3
CJC0512 <ehdchs2006@gmail.com>
2024-04-12 10:39:32 +0900

    확인용 push

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -35,6 +35,7 @@ class CalculatorServiceTests {
     @MethodSource("provideDTOSource")
     void testPlus(CalculatorDTO input) {
 
+        
         // when
         int expected = input.getNum1() + input.getNum2();
 
