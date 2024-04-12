d4b1b1bc5df6dc1828a0a20cfec2594d4f20372e
CJC0512 <ehdchs2006@gmail.com>
2024-04-12 09:30:24 +0900

    pipeline 구축 후 push

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -25,7 +25,8 @@ class CalculatorServiceTests {
         return Stream.of(
             Arguments.of(new CalculatorDTO(1, 2)),
             Arguments.of(new CalculatorDTO(3, 5)),
-            Arguments.of(new CalculatorDTO(10, -2))
+            Arguments.of(new CalculatorDTO(10, -2)),
+            Arguments.of(new CalculatorDTO(1, 5))
         );
     }
 
